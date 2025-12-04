20168d94e8c4b1f4bffeb5a909fcdbe52d9b8392
shreyanshsoni <shreyanshsoni9408@gmail.com>
2025-12-04 21:10:00 +0530

    env debug

--- src/app/api/debug/env/route.ts
+++ src/app/api/debug/env/route.ts
@@ -6,7 +6,7 @@ export async function GET() {
     AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
     hasDatabaseUrl: !!process.env.DATABASE_URL,
-    hasPostgresUrl: !!process.env.POSTGRES_URL,
+    hasPostgresUrl: !!process.env.POSTGRES_URL, 
   });
 }
 
